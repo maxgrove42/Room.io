729696f25b7ed84559bc450f316e24a288c90713
maxgrove42 <131721751+maxgrove42@users.noreply.github.com>
2024-04-16 07:24:06 -0400

    begin work on advanced search criteria
    need to fix apartmentbuilding table add-in (join it in the query) and determine what to do with amenities

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ config.read('resources/config.properties')
 # deal with session['first name'] in sql rather than
 #   in session
 # add a page to show user's interests
-# display pet allowances on a unit by unit basis in apartment search
 # TODO ################################################
 
 # Flask Parameters
@@ -171,16 +170,52 @@ def home():
 
 @app.route(search_units_page)
 def search_units():
-    return render_template(html[search_units_page])
+    cursor = conn.cursor()
+    amenities_query = ('''
+                        SELECT distinct aType
+                        FROM Amenities
+                       ''')
+    cursor.execute(amenities_query)
+    amenities = cursor.fetchall()
+
+    building_query = ('''
+                        SELECT distinct buildingName
+                        FROM ApartmentBuilding
+                       ''')
+    cursor.execute(building_query)
+    buildings = cursor.fetchall()
+
+    company_query = ('''
+                        SELECT distinct companyName
+                        FROM ApartmentBuilding
+                       ''')
+    cursor.execute(company_query)
+    companies = cursor.fetchall()
+
+    return render_template(html[search_units_page],
+                           amenities=amenities,
+                           companies=companies,
+                           buildings=buildings)
 
 
 @app.route(unit_results_page, methods=['GET', 'POST'])
 def show_results():
     username = session['username']
-    building = request.form['building']
-    company = request.form['company']
+    building = request.form.getlist('building')
+    company = request.form.getlist('company')
+    zip_code = request.form['zipcode']
+    city = request.form['city']
+    state = request.form['state']
+    min_bedrooms = request.form['minBedrooms']
+    max_bedrooms = request.form['maxBedrooms']
+    min_bathrooms = request.form['minBathrooms']
+    max_bathrooms = request.form['maxBathrooms']
+    amenity = request.form.getlist('desiredAmenities')
 
     cursor = conn.cursor()
+
+    # TODO ######################################################################
+    # need to add in building name / company name table and amenity list somehow.
     query = ('''
                 WITH bedroomCount AS (
                     SELECT unitRentId, COUNT(*) AS bedroomCount
@@ -217,13 +252,69 @@ def show_results():
                 LEFT JOIN petAllowances pa
 					ON pa.companyName = au.companyName
                     AND pa.buildingName = au.buildingName
-                WHERE au.buildingName LIKE %s AND au.companyName LIKE %s
+                -- including this so we can add-on conditions to the where clause as needed
+                WHERE 1 = 1 
             ''')
-    cursor.execute(query, (username, '%' + building + '%', '%' + company + '%'))
+
+    parameters = [username]
+
+    query += create_list_parameter_query('ab.buildingName', building)
+    parameters += building
+
+    query += create_list_parameter_query('ab.companyName', company)
+    parameters += company
+
+    query += create_single_parameter_query('AddrZipCode', zip_code)
+    if zip_code != '': parameters.append(zip_code)
+
+    query += create_single_parameter_query('AddrCity', city)
+    if city != '': parameters.append(city)
+
+    query += create_single_parameter_query('AddrState', state)
+    if state != '': parameters.append(state)
+
+    # query += create_list_parameter_query('aType', amenity)
+    # parameters += amenity
+
+    # handle the min and max bathrooms manually
+    if min_bedrooms != '':
+        query += ' AND bedroomCount >= %s '
+        parameters.append(min_bedrooms)
+    if max_bedrooms != '':
+        query += ' AND bedroomCount <= %s '
+        parameters.append(max_bedrooms)
+    if min_bathrooms != '':
+        query += ' AND bathroomCount >= %s '
+        parameters.append(min_bathrooms)
+    if max_bathrooms != '':
+        query += ' AND bathroomCount <= %s '
+        parameters.append(max_bathrooms)
+
+    cursor.execute(query, tuple(parameters))
     data = cursor.fetchall()
     cursor.close()
 
     return render_template(html[unit_results_page], data=data)
+
+
+def create_list_parameter_query(columnName, list):
+    query = ''
+    if len(list) >= 1:
+        placeholders = ', '.join(['%s'] * len(list))
+        query = f' AND {columnName} IN ({placeholders}) '
+    return query
+
+
+def create_single_parameter_query(columnName, parameter):
+    query = ''
+    if parameter != '':
+        query = f' AND {columnName} = %s '
+    return query
+
+
+def make_list_parameters(database_column, list):
+    if len(list) >= 1:
+        placeholders = ', '.join(['%s'] * len(list))
 
 
 @app.route(show_details_page)
